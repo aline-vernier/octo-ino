bc4abda4d3d9fd586a41e3af9ad8a0843c3b5964
aline <aline.vernier@ensta.fr>
2014-03-18 22:58:36 +0100

    Finalised comments & cleared relics

--- Radioprotection Project/CheckStation/CheckStation.ino
+++ Radioprotection Project/CheckStation/CheckStation.ino
@@ -1,12 +1,12 @@
 
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <SPI.h>        
 
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 79, 100);
-int i, j, k;
+int i;
 
 
 byte state[2]  = {0, 0};
@@ -20,9 +20,12 @@ void setup() {
   Ethernet.begin(mac,ip);
   Udp.begin(5005);
   
-//  Serial.begin(9600); //pour tests
-  
-  /* {buttonPin = 2, emergencyPin = 3, greenLedPin = 4, orangeLedPin = 5, redLedPin = 6, alarm1Pin = 7, alarm2Pin = 8 }*/
+/*******************************************
+bouton de ronde = 2, "arret" = 3, 
+vert = 4, orange = 5, rouge = 6, 
+alarme1 = 7, alarme2 = 8.
+********************************************/
+
   pinMode(2, INPUT);
   pinMode(3, INPUT);
   for(i=4; i<9; i++){
@@ -60,39 +63,50 @@ presse trop longtemps.
    
   {
     Udp.read(state, UDP_TX_PACKET_MAX_SIZE);
-    
+
+/*******************************************
+Ecriture de l'etat local dans le tableau 
+a renvoyer + calcul checksum
+********************************************/   
     for(i=0;i<2;i++){
       bitWrite(state[0], i, here[i]);
       chkSm = chkSm + bitRead(state[0], i);
     }
+    
+/*******************************************
+Ecriture de l'etat du systeme sur les LED
++ calcul checksum
+********************************************/      
     for(i=4; i<9; i++){
       digitalWrite(i, bitRead(state[0], i-2));
       chkSm = chkSm + bitRead(state[0], i-2);
-    }/*Ecriture de l'etat du systeme sur les LED*/
-    
-    bitWrite(state[1], 7, chkSm%2);
-    
-    for(i=0;i<2;i++){
-      for(j=0;j<8;j++){
-        Serial.print(bitRead(state[i], j));
-      }
     }
-    Serial.println("");
-    
+/*******************************************
+Ecriture checksum
+********************************************/     
+    bitWrite(state[1], 7, chkSm%2);
+
+/*******************************************
+Envoi etat local
+********************************************/    
     Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
     Udp.write(state, 2);
     Udp.endPacket();
+
+/*******************************************
+Remise a zero de l'etat local
+********************************************/
 
     for(i=0; i<2; i++){
       hereNoDup[i] = here[i];
       here[i] = 0;
     } 
 
- }//end of if(packetsize){}
+ }//fin de if(packetsize){}
 
  
  
-}//end of loop
+}//fin de loop()
 
 
 
